9e73ace4c0ff102e8d1fcf32f0560a87f80a18c2
Chaman-preet <chaman.preet@evalueserve.com>
2017-08-22 13:10:22 +0530

    commiting through git bash

---
+++ C data/git/Syngenta workspace/Syngenta/src/graph_poc/graph1.java
@@ -0,0 +1,10 @@
+package graph_poc;
+
+public class graph1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Another class added through git");
+	}
+
+}
